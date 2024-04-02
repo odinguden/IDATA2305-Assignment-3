38094a3a09937a90f1e543b085f7291a61b3dd32
sigvebjo <sigve@bjorkedal.org>
2024-04-02 15:05:43 +0200

    Fixed main

--- Main.java
+++ Main.java
@@ -1,11 +1,22 @@
-import java.util.ArrayList;
 import java.util.Random;
 
 public class Main {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
+		System.out.println("Running Preemptive Priority Scheduling");
+		PriSim priSim = new PriSim(getProcesses(10));
+		priSim.start();
+
+		priSim.join();
+
+		System.out.println("Running First Come First Served Scheduling");
+		FirstComeFirstServed firstComeFirstServed = new FirstComeFirstServed(getProcesses(10));
+		firstComeFirstServed.start();
+	}
+
+	public static ScheduledProcess[] getProcesses(int processCount) {
 		Random random = new Random();
-		ScheduledProcess[] processes = new ScheduledProcess[6];
+		ScheduledProcess[] processes = new ScheduledProcess[processCount];
 		for (int i = 0; i < processes.length; i++) {
 			//processes[i] = new ScheduledProcess(i, 1, 1);
 			int arrivalTime = random.nextInt(1, 100);
@@ -14,7 +25,6 @@ public class Main {
 			processes[i] = new ScheduledProcess(i, arrivalTime, burstTime, pri);
 			System.out.printf("PID: %d%n\tArrival: %d%n\tBurst: %d%n", i, arrivalTime, burstTime);
 		}
-
-		new PriSim(processes).start();
+		return processes;
 	}
 }
