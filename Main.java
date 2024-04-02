53517e970475955640248f52b8994d66b80fefdf
Tony Nhat Huy Pham <Tony.n.h.pham@hotmail.com>
2024-04-02 11:46:01 +0200

    Getter and setters

--- Main.java
+++ Main.java
@@ -3,4 +3,12 @@ public class Main {
 	public static void main(String[] args) {
 		new Proccess(0, 0, 0);
 	}
+
+
+
+
+
+
+
+
 }
--- Proccess.java
+++ Proccess.java
@@ -8,7 +8,7 @@ public class Proccess {
 	private double arrivalTime;
 	private double burstTime;
 	private int priotity;
-	
+
 	public Proccess(int id, double arrivalTime, double burstTime, int priotity) {
 		this.id = id;
 		this.arrivalTime = arrivalTime;
@@ -21,6 +21,39 @@ public class Proccess {
 		this.arrivalTime = arrivalTime;
 		this.burstTime = burstTime;
 		this.priotity = 0;
+	}
+
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public double getArrivalTime() {
+		return arrivalTime;
+	}
+
+	public void setArrivalTime(double arrivalTime) {
+		this.arrivalTime = arrivalTime;
+	}
+
+	public double getBurstTime() {
+		return burstTime;
+	}
+
+	public void setBurstTime(double burstTime) {
+		this.burstTime = burstTime;
+	}
+
+	public int getPriotity() {
+		return priotity;
+	}
+
+	public void setPriotity(int priotity) {
+		this.priotity = priotity;
 	}
 
 
