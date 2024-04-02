5b477aeb5e12a0f6a71e9b7e2d5065e7cec84abd
sigvebjo <sigve@bjorkedal.org>
2024-04-02 12:58:29 +0200

    I FIXED THIS

--- Main.java
+++ Main.java
@@ -4,6 +4,7 @@ import java.util.Random;
 public class Main {
 
 	public static void main(String[] args) {
+<<<<<<< Updated upstream
 		new Process(0, 0, 0);
 	}
 
@@ -28,9 +29,16 @@ public class Main {
 					random.nextDouble(10)
 					);
 			}
+=======
+		Random random = new Random();
+		ScheduledProcess[] processes = new ScheduledProcess[6];
+		for (int i = 0; i < processes.length; i++) {
+			//processes[i] = new ScheduledProcess(i, 1, 1);
+			processes[i] = new ScheduledProcess(i, random.nextInt(1, 100), random.nextInt(1, 100));
+>>>>>>> Stashed changes
 		}
 
-		return proccesses;
+		new FirstComeFirstServed(processes).start();
 	}
 
 }
